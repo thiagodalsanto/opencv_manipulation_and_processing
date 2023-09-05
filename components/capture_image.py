d8046d7a0331ae2a21400eaed75f114b72be7e91
Thiago Dal Santo <55465433+thiagodalsanto@users.noreply.github.com>
2023-09-05 14:38:29 -0300

    Update capture_image.py

--- components/capture_image.py
+++ components/capture_image.py
@@ -4,7 +4,7 @@ class CaptureImage:
     def __init__(self, main_instance):
         self.main_instance = main_instance
 
-    def color_capture(self, event, x, y, flags, param):
+    def capture_color(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             pixel_color = self.main_instance.captured_image[y, x]
-            print(f"Cor RGB: {pixel_color}")
+            print(f"Color RGB: {pixel_color}")
