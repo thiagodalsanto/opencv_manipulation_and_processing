74e6dda1216f9f6655994f89ad1d46a12286fafc
Thiago Dal Santo <55465433+thiagodalsanto@users.noreply.github.com>
2023-09-05 14:38:53 -0300

    Update video_processing.py

--- components/video_processing.py
+++ components/video_processing.py
@@ -9,12 +9,12 @@ from components.capture_image import CaptureImage
 class Main:
     def __init__(self, video_filename):
         self.video_filename = video_filename
-        self.imagem_capturada = None
+        self.captured_image = None
         self.apply_filters = ApplyFilters(self)
         self.capture_image = CaptureImage(self)
-        self.brilho_atual = 0
-        self.sigma_atual = 0
-        self.kernel_size_atual = 1
+        self.brightness_now = 0
+        self.actual_sigma = 0
+        self.actual_kernel = 1
         self.lower_limiar = 1
         self.upper_limiar = 1
         self.threshold = 1
@@ -27,18 +27,17 @@ class Main:
 
         def create_capture_window():
             root = tk.Tk()
-            root.title("Capturar")
+            root.title("Capture")
             root.geometry("200x100")
 
             def capture_image():
                 self.capture_button_pressed = True
 
-            capture_button = ttk.Button(root, text="Capturar", command=capture_image)
+            capture_button = ttk.Button(root, text="Capture", command=capture_image)
             capture_button.pack(pady=20)
 
             root.mainloop()
 
-        # Cria a thread para a janela do botão "Capturar"
         capture_thread = threading.Thread(target=create_capture_window)
         capture_thread.daemon = True
         capture_thread.start()
@@ -54,18 +53,17 @@ class Main:
             tecla = cv2.waitKey(1) & 0xFF
 
             if self.capture_button_pressed:
-                cv2.destroyWindow("Imagem Capturada")
-                self.imagem_capturada = frame_video.copy()
-                self.imagem_capturada = cv2.cvtColor(self.imagem_capturada, cv2.COLOR_BGR2RGB)
-                cv2.imshow("Imagem Capturada", self.imagem_capturada)
+                self.captured_image = frame_video.copy()
+                self.captured_image = cv2.cvtColor(self.captured_image, cv2.COLOR_BGR2RGB)
+                cv2.imshow("Captured", self.captured_image)
 
-                cv2.setMouseCallback("Imagem Capturada", self.capture_image.capturar_cor)
+                cv2.setMouseCallback("Captured", self.capture_image.capture_color)
 
-                cv2.createTrackbar("Gaussian Blur", "Imagem Capturada", self.sigma_atual, 255, self.apply_filters.gaussian_blur)
-                cv2.createTrackbar("Bilateral Blur", "Imagem Capturada", self.kernel_size_atual, 255, self.apply_filters.bilateral_blur)
-                cv2.createTrackbar("Lower Limiar", "Imagem Capturada", self.lower_limiar, 255, self.apply_filters.change_lower_limiar)
-                cv2.createTrackbar("Upper Limiar", "Imagem Capturada", self.upper_limiar, 255, self.apply_filters.change_upper_limiar)
-                cv2.createTrackbar("Threshold", "Imagem Capturada", self.threshold, 255, self.apply_filters.apply_threshold)
+                cv2.createTrackbar("Gaussian Blur", "Captured", self.actual_sigma, 255, self.apply_filters.gaussian_blur)
+                cv2.createTrackbar("Bilateral Blur", "Captured", self.actual_kernel, 255, self.apply_filters.bilateral_blur)
+                cv2.createTrackbar("Lower Limiar", "Captured", self.lower_limiar, 255, self.apply_filters.change_lower_limiar)
+                cv2.createTrackbar("Upper Limiar", "Captured", self.upper_limiar, 255, self.apply_filters.change_upper_limiar)
+                cv2.createTrackbar("Threshold", "Captured", self.threshold, 255, self.apply_filters.apply_threshold)
 
                 self.capture_button_pressed = False
 
@@ -73,5 +71,5 @@ class Main:
         cv2.destroyAllWindows()
 
     def update_limiar_filter(self):
-        edges = cv2.Canny(self.imagem_capturada, self.lower_limiar, self.upper_limiar, apertureSize=3, L2gradient=False)
-        cv2.imshow("Imagem Capturada", edges)
+        edges = cv2.Canny(self.captured_image, self.lower_limiar, self.upper_limiar, apertureSize=3, L2gradient=False)
+        cv2.imshow("Captured", edges)
