a02e03b399b2df4e0414b8bb8b84acaacc9194d1
Thiago Dal Santo <55465433+thiagodalsanto@users.noreply.github.com>
2023-09-05 14:38:19 -0300

    Update apply_filters.py

--- components/apply_filters.py
+++ components/apply_filters.py
@@ -6,19 +6,19 @@ class ApplyFilters:
         self.main_instance = main_instance
 
     def apply_filters(self, gaussian_value, bilateral_value):
-        color_channels = cv2.split(self.main_instance.imagem_capturada)
+        color_channels = cv2.split(self.main_instance.captured_image)
         self.main_instance.brightness_now = gaussian_value
 
-        gaussian_image = cv2.GaussianBlur(color_channels[0], (5, 5), gaussian_value)
+        imagem_gaussian = cv2.GaussianBlur(color_channels[0], (5, 5), gaussian_value)
 
         bilateral_image = cv2.bilateralFilter(color_channels[0], 25, bilateral_value, bilateral_value)
-        result_image = cv2.addWeighted(gaussian_image, 0.7, bilateral_image, 0.3, 0)
+        result_image = cv2.addWeighted(imagem_gaussian, 0.7, bilateral_image, 0.3, 0)
         mod_image = cv2.add(result_image, np.ones_like(color_channels[0]) * self.main_instance.brightness_now)
 
         cv2.imshow("Captured", mod_image)
 
     def gaussian_blur(self, value):
-        self.apply_filters(value, self.main_instance.actual_kernel_size)
+        self.apply_filters(value, self.main_instance.actual_kernel)
 
     def bilateral_blur(self, value):
         self.apply_filters(self.main_instance.actual_sigma, value)
@@ -32,12 +32,12 @@ class ApplyFilters:
         self.main_instance.update_limiar_filter()
 
     def apply_threshold(self, value):
-        if self.main_instance.imagem_capturada is not None:
-            imagem_azul = self.main_instance.imagem_capturada[:, :, 0]
+        if self.main_instance.captured_image is not None:
+            blue_image = self.main_instance.captured_image[:, :, 0]
 
-            blue_image_8bits = cv2.convertScaleAbs(imagem_azul)
+            blue_image_8bits = cv2.convertScaleAbs(blue_image)
 
-            _, binary_image = cv2.threshold(blue_image_8bits, value, 255, cv2.THRESH_BINARY)
-            cv2.imshow("Captured", binary_image)
+            _, imagem_binaria = cv2.threshold(blue_image_8bits, value, 255, cv2.THRESH_BINARY)
+            cv2.imshow("Captured", imagem_binaria)
 
 
